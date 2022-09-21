d259a420f7241bc8300ff29cb938be2b501f2c2d
macscEE <bax.random@gmail.com>
2022-09-21 14:03:53 +0200

    fix timekeeper
    works fine now

--- programming/2tx_1rx_wMenu/rx_menu/rx_menu.ino
+++ programming/2tx_1rx_wMenu/rx_menu/rx_menu.ino
@@ -20,23 +20,26 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-RF24 radio(CE_PIN, CSN_PIN);
+RF24 radio(CE_PIN, CSN_PIN);  //Vari nodi del sistema di trasmissione
 RF24Network network(radio);
 const uint16_t nodeRX = 0;
 const uint16_t nodeTX1 = 1;
 const uint16_t nodeTX2 = 2;
 
-int dataReceived;      // this must match dataToSend in the TX
+bool debounce = false;  //Uso come antirimbalzo
+int dataReceived; // invio un int così dal numero so che fotocellula ha inviato
 bool startSend = true; // invio quando voglio iniziare far attivare i TX
 
-bool done = false; // invio un int così dal numero so chi lo ha inviato
+bool done = false;
 long inTime = 0;
 long cuTime = 0;
-//long tempTime;
 
 bool flag = true;
 int count = 0;    // Conta le pressioni del pulsante e quindi i vari programmi del menu
 int foto;         // per selezionare che fotocellula usare nel caso se ne usi una sola
+
+int t1i, t1d, t2i, t2d, ti, td; //Var. per memorizzare i tempi anche come stringhe
+String t1si, t1sd, t2si, t2sd, tsi, tsd;
 
 void setup()
 {
@@ -46,16 +49,16 @@ void setup()
   radio.begin();
   network.begin(124, nodeRX); // canale, indirizzo nodo
   radio.setDataRate(RF24_250KBPS);
-  // inTime = millis();
 
   pinMode(startButton, INPUT);
+  pinMode(soundStart, OUTPUT);
 
-  // For LCD
+  // For menu LCD
   pinMode(buttonSCOR, INPUT);
   pinMode(buttonOK, INPUT);
+
   lcd.begin(16, 2);
   lcd.backlight(); // accendo retroilluminazione
-  // Print a message to the LCD.
   lcd.print("Benvenuto!");
   delay(1500);
   lcd.clear();
@@ -64,15 +67,25 @@ void setup()
   lcd.print("programma");
   delay(1500);
   lcd.clear();
+
   while (flag)
   {
     if (digitalRead(buttonSCOR) == LOW)
     {
-      count++;
-      lcd.clear(); // Cambia il programma quindi cancello schermo per leggere
-      if (count == 2)
-        count = 0;
+      if (!debounce)
+      {
+        count++;
+        lcd.clear(); // Cambia il programma quindi cancello schermo per leggere
+        if (count == 2)
+          count = 0;
+        debounce = true;
+      }
     }
+    else
+    {
+      debounce = false;
+    }
+
     switch (count)
     {
       case 0:
@@ -92,14 +105,18 @@ void setup()
         lcd.print("Posizionare le");
         lcd.setCursor(0, 1);
         lcd.print("fotocellule");
+
         delay(3000);
+
         lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Premi OK");
+        lcd.setCursor(0, 1);
+        lcd.print("per avanzare");
+
         while (digitalRead(buttonOK) == HIGH)
         {
-          lcd.setCursor(0, 0);
-          lcd.print("Premi OK");
-          lcd.setCursor(0, 1);
-          lcd.print("per avanzare");
+          //Attendo
         }
       }
 
@@ -112,13 +129,22 @@ void setup()
         lcd.print("fotocellula");
         delay(1500);
         lcd.clear();
+
         foto = 0; // Variabile temporanea di conteggio
         while (digitalRead(buttonOK) == HIGH)
         {
           if (digitalRead(buttonSCOR) == LOW)
           {
-            foto++;
-            lcd.clear();
+            if (!debounce)
+            {
+              foto++;
+              lcd.clear();
+              debounce = true;
+            }
+          }
+          else
+          {
+            debounce = false;
           }
           if (foto == 2)
             foto = 0;
@@ -159,24 +185,29 @@ void loop()
   {
     if (digitalRead(startButton) == LOW)
     {
-      Serial.println("Pronti..");
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Pronti..");
+
       digitalWrite(soundStart, HIGH);
-      delay(300);
+      delay(800);
       digitalWrite(soundStart, LOW);
-      int wait = random(2000, 3500); //persona ora sul "pronti"
+
+      int wait = random(3000, 5000); //persona ora sul "pronti"
       delay(wait);  //attesa in millisecondi
       digitalWrite(soundStart, HIGH);  //segnalo con il led lo sparo
+
       inTime = millis();
-      delay(300);
+
+      delay(600);
       digitalWrite(soundStart, LOW);
+
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Acquisisco..");
       Serial.println("Partenza cronometro");
-      sendData();
+
+      sendData(); //Faccio trasmettere le fotocellule
       done = true;
     }
   }
@@ -245,44 +276,45 @@ void getTime()
 String empty = String(); // Creo un oggetto stringa vuota
 String sd = String();    // Dove metto le cifre dopo la virgola
 String si = String();    // cifre prima della virgola
+
 void t1()
 {
   if (count == 0)
   {
     if (foto == 0)
     {
-      //tempTime = inTime;
-      timeCalc(inTime);
-      Serial.println("Tempo: " + si + "." + sd);
+      timeCalc();
+
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Tempo: " + si + "." + sd);
+      lcd.print("Tempo: " + tsi + "." + tsd);
       lcd.setCursor(0, 1);
       lcd.print("OK per reset");
+
       while (digitalRead(buttonOK) == HIGH)
       {
         //Aspetto che venga premuto il pulsante
       }
-      dataReceived = 0;
-      done = false; // Riazzero la procedura dato che devo prendere solo un tempo
+
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Premere START");
       lcd.setCursor(0, 1);
       lcd.print("per iniziare");
+
+      dataReceived = 0;
+      done = false; // Riazzero la procedura dato che devo prendere solo un tempo
     }
   }
   else
   {
-    //tempTime = inTime;
-    timeCalc(inTime);
-    Serial.println("Tempo 1° parz.: " + si + "." + sd);
-    //tempTime = cuTime;
-    dataReceived = 0;
+    timeCalc();
 
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("T1: " + si + "." + sd);
+
+    dataReceived = 0;
   }
 }
 
@@ -293,44 +325,60 @@ void t2()
   {
     if (foto == 1)
     {
-      //tempTime = inTime;
-      timeCalc(inTime);
-      Serial.println("Tempo: " + si + "." + sd);
+      timeCalc();
+
       lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Tempo: " + tsi + "." + tsd);
+      lcd.setCursor(0, 1);
+      lcd.print("OK per reset");
+
       while (digitalRead(buttonOK) == HIGH)
       {
-        lcd.setCursor(0, 0);
-        lcd.print("Tempo: " + si + "." + sd);
-        lcd.setCursor(0, 1);
-        lcd.print("OK per reset");
+        //Aspetto di ricevere conferma per avanzare
       }
+
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Premere START");
       lcd.setCursor(0, 1);
       lcd.print("per iniziare");
+
       dataReceived = 0;
       done = false; // Riazzero la procedura dato che devo prendere solo un tempo
     }
   }
   else
   {
+    timeCalc();
+
     lcd.clear();
     lcd.setCursor(0, 0);
-    Serial.println("Tempo 2° parz.: " + si + "." + sd);
-    lcd.print("Totale: " + si + "." + sd);
-    Serial.println("Totale: " + si + "." + sd);
+    lcd.print("Totale: " + tsi + "." + tsd);
+    lcd.setCursor(0, 1);
+    lcd.print("SCOR per T1, T2");
+
+    calcT2();
+
     int temp = -1; //variabile di conteggio temporaneo
-    
+
     while (digitalRead(buttonOK) == HIGH)
     {
       if (digitalRead(buttonSCOR) == LOW)
       {
-        temp++;
-        lcd.clear();
+        if (!debounce)
+        {
+          temp++;
+          lcd.clear();
+          if (temp == 2)
+            temp = 0;
+          debounce = true;
+        }
       }
-      if (temp == 2)
-        temp = 0;
+      else
+      {
+        debounce = false;
+      }
 
       if (temp == 0)
       {
@@ -343,24 +391,27 @@ void t2()
       if (temp == 1)
       {
         lcd.setCursor(0, 0);
-        lcd.print("Totale: " + si + "." + sd);
-        lcd.setCursor(0,1);
+        lcd.print("Totale: " + tsi + "." + tsd);
+        lcd.setCursor(0, 1);
         lcd.print("OK per reset");
       }
     }
-    //tempTime = cuTime;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Premere START");
+    lcd.setCursor(0, 1);
+    lcd.print("per iniziare");
+
     dataReceived = 0;
     done = false; // Faccio ripartire la procedura di partenza
   }
 }
 
 
-int t1i, t1d, t2i, t2d, ti, td;
-String 
-void timeCalc(long tempTime)
+void timeCalc()
 {
   cuTime = millis();
-  long rslt = (cuTime - tempTime);
+  long rslt = (cuTime - inTime);
   long mills = (rslt % 1000);
   long seconds = (rslt / 1000);
   sd = empty + mills; // Creo una stringa che mi serve in caso debba aggiungere uno "0" ai millesimi
@@ -375,5 +426,62 @@ void timeCalc(long tempTime)
     sd = "0" + sd;
     if (mills < 10)
       sd = "0" + sd; // Aggiungo un altro zero se è minore di 10, tipo "005"
+  }
+
+  if (count == 1)
+  {
+    if (dataReceived == 1) //T1
+    {
+      t1i = seconds; //Interi
+      t1d = mills;
+
+      t1si = si;  //Stringhe
+      t1sd = sd;
+    }
+    else  //dataReceived == 2 || Totale, quindi ho T1 + T2
+    {
+      ti = seconds; //Interi
+      td = mills;
+
+      tsi = si; //Stringhe
+      tsd = sd;
+    }
+  }
+  else
+  {
+    tsi = si; //Stringhe
+    tsd = sd;
+
+  }
+}
+
+void calcT2()
+{
+  //Calcolo i secondi
+  t2i = ti - t1i;
+
+  if (td < t1d)
+  {
+    t2d = (1000 + td) - t1d;  //Prendo in prestito un secondo = 1000 millisecondi
+    t2i--;
+  }
+  else
+  {
+    //Calcolo i decimi
+    t2d = td - t1d;
+  }
+
+  t2si = t2i + empty; //Trasformo in stringa per scrivere su display
+  t2sd = t2d + empty;
+
+  if (t2i > 99)
+    t2i = 99;
+  if (t2i < 10)
+    t2si = "0" + t2si;
+  if (t2d < 100)
+  {
+    t2sd = "0" + t2sd;
+    if (t2d < 10)
+      t2sd = "0" + t2sd; // Aggiungo un altro zero se è minore di 10, tipo "005"
   }
 }
